7162ee9c43a8fdbb6b04b60e03567ad100b08c82
Vinaysingh09 <98451993+Vinaysingh09@users.noreply.github.com>
2022-01-28 22:20:26 +0530

    Update UserController.java

--- src/main/java/com/scma/anilg/controller/UserController.java
+++ src/main/java/com/scma/anilg/controller/UserController.java
@@ -114,7 +114,7 @@ public class UserController {
 				originalFilename = currDateTime+"@"+mpFile.getOriginalFilename();
 			}	
 				/** retrieve current class-path resource folder relative path */
-				 				 String savedFile = "system:jar:file:/app/target/classes/static/image/"; 
+				 				 String savedFile = "app/target/classes/static/image/"; 
 			 
 				 destPath = Paths.get(savedFile +File.separator+originalFilename);
 				 
